3f11dfd7294c8c76d293185d51096e5a14205c08
MohammadReza Ghadimi <mmr.ghadimi@gmail.com>
2021-10-06 19:04:29 +0330

    my coomp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a yes baby i changed it
+# This is a yes baby i changed it on my computer
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
